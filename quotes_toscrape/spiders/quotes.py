0119802968a38540c7e4b12d3b3af0d7922a4b88
Fateme Fouladkar <fatemefuoladkar@gmail.com>
2023-01-01 13:58:52 +0330

    first page scraper added

--- quotes_toscrape/spiders/quotes.py
+++ quotes_toscrape/spiders/quotes.py
@@ -1,4 +1,5 @@
 import scrapy
+import datetime
 
 
 class QuotesSpider(scrapy.Spider):
@@ -6,5 +7,20 @@ class QuotesSpider(scrapy.Spider):
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 50,
+        'DOWNLOAD_DELAY': 0.1,
+        'FEED_URI': f'output/quotes_{datetime.datetime.today().strftime("%Y-%m-%d %H-%M-%S")}.csv',
+        'FEED_FORMAT': 'csv',
+        'FEED_EXPORTERS': {'csv': 'scrapy.exporters.CsvItemExporter'},
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'FEED_EXPORT_FIELDS': ('text', 'author', 'tags') 
+    }
+
     def parse(self, response):
-        pass
+        for quote in response.xpath("//div[@class='quote']"):
+            yield {
+                'text': quote.xpath(".//span[@class='text']/text()").get(),
+                'author': quote.xpath(".//small[@class='author']/text()").get(),
+                'tags': quote.xpath(".//a[@class='tag']/text()").getall(),
+                }
