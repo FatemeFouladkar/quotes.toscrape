07e249214dd721100a2cfe19a4bd36d288de649b
Fateme Fouladkar <fatemefuoladkar@gmail.com>
2023-01-04 14:43:44 +0330

    url issue in authors spider fixed

--- quotes_toscrape/spiders/authors.py
+++ quotes_toscrape/spiders/authors.py
@@ -1,13 +1,12 @@
 import scrapy
 import datetime
-
 class AuthorsSpider(scrapy.Spider):
     name = 'authors'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
     custom_settings = {
-        # 'CONCURRENT_REQUESTS': 50,
-        # 'DOWNLOAD_DELAY': 0.1,
+        'CONCURRENT_REQUESTS': 50,
+        'DOWNLOAD_DELAY': 0.1,
         'FEED_URI': f'output/authors_{datetime.datetime.today().strftime("%Y-%m-%d %H-%M-%S")}.csv',
         'FEED_FORMAT': 'csv',
         'FEED_EXPORTERS': {'csv': 'scrapy.exporters.CsvItemExporter'},
@@ -16,9 +15,11 @@ class AuthorsSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        for _ in response.xpath("//div[@class='quote']"):
-            author_page = response.xpath("//a[text()='(about)']/@href").get()
-            yield response.follow(author_page, callback=self.parse_author)
+        for author in response.xpath("//div[@class='quote']/span/a/@href").getall():
+            yield scrapy.Request(response.urljoin(author)+'/',
+                                method="GET",
+                                callback=self.parse_author,
+                                headers={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36'})
 
         next_page = response.xpath("//li[@class='next']/a/@href").get()
         if next_page:
@@ -26,13 +27,9 @@ class AuthorsSpider(scrapy.Spider):
 
 
     def parse_author(self, response):
-        print("------------------------------------")
         yield {
             'name': response.xpath("//h3[@class='author-title']/text()").get(),
             'birth_date': response.xpath("//span[@class='author-born-date']/text()").get(),
             'birth_location': response.xpath("//span[@class='author-born-location']/text()").get(),
             'description': response.xpath("//div[@class='author-description']/text()").get()
         }
-
-
-
